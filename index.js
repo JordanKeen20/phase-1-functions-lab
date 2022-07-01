08514fdea60f284e23dd49fadb8d9bf1667f0c01
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-07-01 02:15:08 -0700

    commit tree b53c67c54743722843b38af55348ac9d67345269

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(startA){
+    if (startA > 42)
+        return startA - 42;
+    else (startA < 42)
+        return 42 - startA;
+}
+
+function distanceFromHqInFeet(startB){
+    if (startB > 42) 
+        return (startB - 42) * 264;
+    else (startB < 42) 
+        return (42 - startB) * 264;
+}
+
+
+function distanceTravelledInFeet(start1,end1){
+    if (start1 > end1)
+        return (start1-end1)*264
+    else (start1 < end1)
+        return (end1-start1)*264
+}
+
+
+function calculatesFarePrice(start2,end2){
+   let miles = Math.abs(start2-end2);
+   let travel = miles * 264;
+   if (travel <= 400){
+        console.log('gives customers a free sample');
+        return 0
+   }  
+   else if (travel >= 2500)
+        return 'cannot travel that far'
+    else if (travel >2000 && travel <= 2500){
+        return 25 }
+    else if (travel>400 && travel <= 2000){
+        let cost = (0.02 * (travel - 400));
+        return cost
+    }
+    }
